e5ed233b4e19d61517e9ca6d8cc9fbd03f4a8dfa
yu-1238 <51786737+yu-1238@users.noreply.github.com>
2021-10-18 18:04:49 +0800

    Delete cmd_test.sh

--- bin/cmd_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-START=11
-END=39
-seq $START 1 $END | \
-    xargs -I {} echo "./updatenet/checkpoint/checkpoint{}.pth.tar" | \
-    xargs -I {} python -u ./bin/my_test.py --update_path {} 
